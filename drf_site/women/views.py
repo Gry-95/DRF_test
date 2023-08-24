e16eacbbebb3a49c8cdaa18a0639e4c31b4942dc
Andrey Grinenko <green37tea@gmail.com>
2023-08-24 15:12:23 +0300

    create CRUD

--- drf_site/women/views.py
+++ drf_site/women/views.py
@@ -23,11 +23,37 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)  # raise_exception=True вернет JSON с ошибкой, а не html с ошибкой
+        serializer.save()
+        # post_new = Women.objects.create(
+        #     title=request.data['title'],
+        #     content=request.data['content'],
+        #     cat_id=request.data['cat_id']
+        # ) # Вместо этого написал метод create в serializers.py
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
+        return Response({'post': serializer.data})
 
-        return Response({'post': WomenSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'Error': 'Method PUT NOT allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'Error': 'Objects does NOT exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'Error': 'Method DELETE NOT allowed'})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'Error': 'Objects does NOT exists'})
+        instance.delete()
+        return Response({'post': 'delete post ' + str(pk)})
